8882d684c7a9f3d258e9c8a55cc5b94180906cc4
MihirJaiswal <jaiswalmihir.business@gmail.com>
2024-10-20 12:50:03 +0530

    some changes

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -7,17 +7,13 @@ import img1 from '../../../public/hero1.webp';
 import img from '../../../public/hero2.webp';
 import img3 from '../../../public/hero3.webp';
 import ZoomedImage from './ZoomImage';
-
 import Image from 'next/image';
 import HeroWord from './Heroword';
-
 
 export default function Component() {
   const images = [img, img1, img2, img3];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [direction, setDirection] = useState(0); // 1 for next, -1 for previous
-
-  
 
   const handlePrevImage = () => {
     setDirection(-1);
@@ -56,17 +52,18 @@ export default function Component() {
   };
 
   return (
-    <div className="relative w-full max-w-7xl mx-auto">
-      <div className="relative  z-10 border-b flex flex-col-reverse lg:flex-row items-cnter justify-between h-full">
-        <div className="lg:w-1/2 border-t w-full lg:h-full flex items-center justify-center py-10 lg:py-0 relative">
+    <div className="relative w-full max-w-7xl mx-auto p-4"> {/* Added background and padding */}
+      <div className="relative z-10  flex flex-col-reverse lg:flex-row items-center justify-between h-full">
+        <div className="lg:w-1/2 w-full lg:h-full flex items-center justify-center py-10 lg:py-0 relative">
           <button
             onClick={handlePrevImage}
-            className="absolute left-4 lg:left-10 z-20 bg-white text-black p-3 rounded-full shadow-lg hover:bg-gray-200"
+            className="absolute left-4 lg:left-10 z-20 bg-white text-black p-3 rounded-full hover:bg-gray-200 transition duration-200 ease-in-out" 
+            aria-label="Previous Image"
           >
             <ArrowLeft className="h-6 w-6" />
           </button>
 
-          <div className="w-[90%] h-[300px] lg:h-[530px] mt-12 overflow-hidden">
+          <div className="w-[90%] h-[300px] lg:h-[530px] mt-12 overflow-hidden rounded-lg">
             <AnimatePresence initial={false} custom={direction}>
               <motion.div
                 key={currentImageIndex}
@@ -76,7 +73,7 @@ export default function Component() {
                 animate="center"
                 exit="exit"
                 transition={transition}
-                className="absolute w-full h-full"
+                className="absolute w-full h-full rounded-lg overflow-hidden" // Added rounded corners
               >
                 <ZoomedImage imgSrc={images[currentImageIndex]} />
               </motion.div>
@@ -85,12 +82,12 @@ export default function Component() {
 
           <button
             onClick={handleNextImage}
-            className="absolute right-4 lg:right-10 z-20 bg-white text-black p-3 rounded-full shadow-lg hover:bg-gray-200"
+            className="absolute right-4 lg:right-10 z-20 bg-white text-black p-3 rounded-full hover:bg-gray-200 transition duration-200 ease-in-out" 
+            aria-label="Next Image"
           >
             <ArrowRight className="h-6 w-6" />
           </button>
         </div>
-      
       </div> 
     </div>
   );
